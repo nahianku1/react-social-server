a511b1e00912c3ee801a725709839b6f80ace162
nahian islam <nahianku1@gmail.com>
2025-05-26 19:07:45 +0600

    5th commit

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import { Server } from "socket.io";
-import http, { get } from "http";
+import http from "http";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World! Again");
